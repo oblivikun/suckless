52c9f6dff289d70ace99e41ca3ea23b25072dd3a
Bradley Taunt <bt@btxx.org>
2024-01-23 15:26:52 -0500

    Update install script and include source suckless programs

--- install.sh
+++ install.sh
@@ -5,43 +5,34 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
-mkdir /home/bt/.suckless
-cd /home/bt/.suckless
-
-git clone https://git.sr.ht/~bt/dwmo
 cd dwmo
 echo "Installing dwm"
 rm -rf config.h
 make clean install
 cd ..
 
-git clone https://git.sr.ht/~bt/dmenuo
 cd dmenuo
 echo "Installing dmenu"
 rm -rf config.h
 make clean install
 cd ..
 
-git clone https://git.sr.ht/~bt/slstatuso
 cd slstatuso
 echo "Installing slstatus"
 rm -rf config.h
 make clean install
 cd ..
 
-git clone https://git.sr.ht/~bt/sto
 cd sto
 echo "Installing st"
 rm -rf config.h
 make clean install
 cd ..
 
-git clone https://git.sr.ht/~bt/slocko
 cd slocko
 echo "Installing slock"
 rm -rf config.h
 make clean install
-cd ..
 cd ..
 
 rcctl enable apmd
